8cb63f8fdccd3a3a50ae5c1e65184f2f91513726
Breno Erik Rebelatto <breno.rebelatto@outlook.com>
2026-07-14 20:26:07 -0300

    Exercicio de validar numero entre 1 e 10 usando While

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿int tabuada = 0;
-int resultado = 0;
+﻿// int tabuada = 0;
+// int resultado = 0;
 
-while( tabuada < 21)
+// while( tabuada < 21)
+// {
+//     Console.WriteLine($"2 x {tabuada} = {resultado}");
+//     tabuada += 1;
+//     resultado = 2 * tabuada;
+// }
+
+int i = 0;
+
+ Console.WriteLine("Digite um número entre 1 e 10: ");
+    i = int.Parse(Console.ReadLine());
+
+while(i < 1 || i > 10)
 {
-    Console.WriteLine($"2 x {tabuada} = {resultado}");
-    tabuada += 1;
-    resultado = 2 * tabuada;
+    Console.WriteLine("Número indisponível, insira novamente:");
+    i = int.Parse(Console.ReadLine());
 }
-
