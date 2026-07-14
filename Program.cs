478a60e53efdfaa05f175842057c050cd92b6794
Breno Erik Rebelatto <breno.rebelatto@outlook.com>
2026-07-14 20:25:22 -0300

    Exercicio da tabuada usando while

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Digite um número: ");
+﻿int tabuada = 0;
+int resultado = 0;
+
+while( tabuada < 21)
+{
+    Console.WriteLine($"2 x {tabuada} = {resultado}");
+    tabuada += 1;
+    resultado = 2 * tabuada;
+}
+
